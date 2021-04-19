3c6ad5cfb56818267b3607abb9df4b551d50ec4b
Jonas <Jogro17@student.sdu.dk>
2021-04-19 11:03:22 +0200

    warning at save btn

--- Audiotoxic/Audiotoxic/Controllers/ProfileViewController.swift
+++ Audiotoxic/Audiotoxic/Controllers/ProfileViewController.swift
@@ -13,33 +13,49 @@ class ProfileViewController: UIViewController {
     let profile = Profile()
     
     @IBAction func save(_ sender: Any) {
+        let refreshAlert = UIAlertController(title: "Warning", message: "All data will be lost if you proceed", preferredStyle: UIAlertController.Style.alert)
 
-        if (name.text != "") {
-            profileSaved.alpha = 1
-            profileSaved.text = ""
-            if sex.selectedSegmentIndex == 0 {
-                gender = true
-            } else {
-                gender = false
+        refreshAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+            //Handle Ok Logic here
+            if (self.name.text != "") {
+                self.profileSaved.alpha = 1
+                self.profileSaved.text = ""
+                if self.sex.selectedSegmentIndex == 0 {
+                    self.gender = true
+                } else {
+                    self.gender = false
+                }
+                
+                self.profile.name = self.name.text!
+                self.profile.sex = self.gender
+                self.profile.dateOfBirth = self.datePick.date.addingTimeInterval(7200.0)
+                self.profile.saveProfile()
+                //Shows the text profilesaved for 2 seconds!
+                UIView.animate(withDuration: 2) {
+                    self.profileSaved.text = "Profile saved!"
+                    self.profileSaved.alpha = 0
+                }
+                
             }
-            
-            profile.name = self.name.text!
-            profile.sex = gender
-            profile.dateOfBirth = self.datePick.date.addingTimeInterval(7200.0)
-            profile.saveProfile()
-            //Shows the text profilesaved for 2 seconds!
+            if let tabBarItem = self.tabBarController?.tabBar.items?[1] {
+                tabBarItem.isEnabled = true
+            }
+            if let tabBarItem = self.tabBarController?.tabBar.items?[3] {
+                tabBarItem.isEnabled = true
+            }
+        }))
+
+        refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+              //Handle Cancel Logic here
             UIView.animate(withDuration: 2) {
-                self.profileSaved.text = "Profile saved!"
+                self.profileSaved.text = "Profile was not saved!"
                 self.profileSaved.alpha = 0
             }
-            
-        }
-        if let tabBarItem = self.tabBarController?.tabBar.items?[1] {
-            tabBarItem.isEnabled = true
-        }
-        if let tabBarItem = self.tabBarController?.tabBar.items?[3] {
-            tabBarItem.isEnabled = true
-        }
+        }))
+
+        present(refreshAlert, animated: true, completion: nil)
+
+       
         //self.navigationController?.dismiss(animated: true, completion: nil)
     }
     override func viewDidAppear(_ animated: Bool) {
