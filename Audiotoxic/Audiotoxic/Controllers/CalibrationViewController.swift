51b01a25e0a7cb6ba18043c624c708aa02149694
Alexander Tholle <altho18@student.sdu.dk>
2021-04-26 18:37:19 +0200

    Added calibration screen, still missing functionality

--- Audiotoxic/Audiotoxic/Controllers/CalibrationViewController.swift
+++ Audiotoxic/Audiotoxic/Controllers/CalibrationViewController.swift
@@ -8,15 +8,58 @@
 
 import UIKit
 
-class CalibrationViewController: UIViewController {
-
+class CalibrationViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var inputDBText: UITextField!
+    @IBOutlet weak var maxDBText: UITextField!
+    @IBOutlet weak var frequencyPicker: UIPickerView!
+    @IBOutlet weak var earControl: UISegmentedControl!
+    
+    let frequencies = ["10000Hz", "12500Hz", "14000Hz", "16000Hz"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.frequencyPicker.delegate = self
+        self.frequencyPicker.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func playAtMax(_ sender: Any) {
+        //Just play sound at max volume on selected ear
+        let leftEar = earControl.selectedSegmentIndex == 0 ? true : false
+        let selectedFrequency = getSelectedFrequency()
+        
+    }
+    
+    @IBAction func playAtInputDB(_ sender: Any) {
+        //Play sound with the given dB level
+        let leftEar = earControl.selectedSegmentIndex == 0 ? true : false
+        let selectedFrequency = getSelectedFrequency()
+        let maxDB = maxDBText.text
+        let inputDB = inputDBText.text
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return frequencies.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return frequencies[row]
+        }
+    
+    private func getSelectedFrequency() -> Int {
+        var selectedFrequency = frequencies[frequencyPicker.selectedRow(inComponent: 0)]
+        selectedFrequency.removeLast()
+        selectedFrequency.removeLast()
+        return Int.init(selectedFrequency)!
+    }
+    
     /*
     // MARK: - Navigation
 
