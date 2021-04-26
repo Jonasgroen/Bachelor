3ee65734cb24a1333ff96afb24605a22791e5bc8
Alexander Tholle <altho18@student.sdu.dk>
2021-04-26 18:36:40 +0200

    Added calibration screen, still missing functionality

---
+++ Audiotoxic/Audiotoxic/Controllers/CalibrationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalibrationViewController.swift
+//  Audiotoxic
+//
+//  Created by Rasmus Bødker on 26/04/2021.
+//  Copyright © 2021 sdu.dk. All rights reserved.
+//
+
+import UIKit
+
+class CalibrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
